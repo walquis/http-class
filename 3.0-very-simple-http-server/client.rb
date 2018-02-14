a67bc1d4695bd3812d0abf2d5d6c63a9560a1020
Chris Walquist <chris@walquist.net>
2018-02-14 14:50:02 -0600

    After reading the spec more closely re the 'Request' ... send server the 2nd CRLF *before* trying to read a response.  I.e., server was rightly waiting for the 'I am done' tag before responding, and client was wrongly reading before finishing its send of the request according to the HTTP format.

--- 3.0-very-simple-http-server/client.rb
+++ 3.0-very-simple-http-server/client.rb
@@ -4,8 +4,8 @@ socket = TCPSocket.open('localhost',8080)
 socket.sync = 1
 socket.print "GET / HTTP/1.1\r\n"
 puts "CLIENT: Sent an HTTP request"
-response_from_server = socket.readline
 socket.print "\r\n"
+response_from_server = socket.readline
 socket.close
 # Status-Line = HTTP-Version SP Status-Code SP Reason-Phrase CRLF
 (ver,stat,reason) = response_from_server.split(' ')
