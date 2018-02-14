a5aa7210de1b583b60359333e0e637366a3bc1db
Chris Walquist <chris@walquist.net>
2018-02-14 14:17:54 -0600

    Commit with broken client that tripped me up in class

--- 3.0-very-simple-http-server/client.rb
+++ 3.0-very-simple-http-server/client.rb
@@ -1,9 +1,13 @@
 require 'socket' # A library built into Ruby - provides the TCPSocket class
 
 socket = TCPSocket.open('localhost',8080)
-socket.print "Hi there, server!\n"
-puts "CLIENT: Sent 'Hi there, server!' to server"
-response_from_server = socket.read
+socket.sync = 1
+socket.print "GET / HTTP/1.1\r\n"
+puts "CLIENT: Sent an HTTP request"
+response_from_server = socket.readline
+socket.print "\r\n"
 socket.close
+# Status-Line = HTTP-Version SP Status-Code SP Reason-Phrase CRLF
+(ver,stat,reason) = response_from_server.split(' ')
 puts "FROM SERVER: " + response_from_server
 puts "CLIENT: Now I'm exiting"
